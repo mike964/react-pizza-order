25085d113c34da4a3c15f434efe3fbf6dd9f3346
Moslm Shlaka <mike7olwo@gmail.com>
2026-01-21 03:01:20 +0300

    PWA config added

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [ react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Fast Pizza',
+        short_name: 'FastPizza',
+        start_url: '.',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'pizza-logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pizza-logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }) 
+  ],
    base: '/react-pizza-order/', // Replace 'your-repo-name'
 })
